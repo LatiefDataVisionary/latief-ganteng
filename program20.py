3e270ae259329866e7d97bf6e93ffbd4760ea53e
Lathif Ramadhan <168287240+LatiefDataVisionary@users.noreply.github.com>
2024-09-11 20:40:15 +0700

    program20

---
+++ program20.py
@@ -0,0 +1,74 @@
+"""
+Write a Python Program to Find Armstrong Number in an Interval.
+"""
+
+# input the interval from the user
+lower = int(input('Enter the lower limit of the interval: '))
+upper = int(input('Enter the upper limit of the interval: '))
+
+# iterate trhough the numbers i
+for num in range(lower, upper + 1): 
+    # Find the number of digits in 'num'
+    order = len(str(num))
+    temp_num = num 
+    sum = 0
+    
+    while temp_num > 0:
+        digit = temp_num % 10
+        sum += digit ** order
+        temp_num //= 10
+        
+    # check if 'num' is an Amstrong number
+    if num == sum:
+        print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
